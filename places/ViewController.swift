a858762965f5c59df218b922df15d7ab24e6ceca
Marko Posio <marko.posio@siili.com>
2018-03-21 23:28:25 +0200

    Add app skeleton

---
+++ places/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  places
+//
+//  Created by Marko Posio on 21/03/2018.
+//  Copyright © 2018 Marko Posio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
